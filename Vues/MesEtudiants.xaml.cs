6af317e52ea35d1fcdb1feef266e9f8b7afa22cd
emmanuelle340 <emmanuellekenmogne340@gmail.com>
2024-01-20 16:50:52 +0100

    update

--- Vues/MesEtudiants.xaml.cs
+++ Vues/MesEtudiants.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CiteUContext = CiteU.Modele.CiteU;
+using System.Runtime.Remoting.Contexts;
 
 namespace CiteU.Vues
 {
@@ -22,6 +24,7 @@ namespace CiteU.Vues
     /// </summary>
     public partial class MesEtudiants : UserControl
     {
+        public CiteUContext context = new CiteUContext();
         public MesEtudiants()
         {
             InitializeComponent();
@@ -31,62 +34,66 @@ namespace CiteU.Vues
         {
             try
             {
-                // Charger le fichier texte en tant que ressource
-                Uri uri = new Uri("pack://application:,,,/CiteU;component/MesEtudiants.txt");
-                var streamInfo = Application.GetResourceStream(uri);
+                string filePath = "MesEtudiants.txt";
+                
+                    var dernierEtudiant = context.Etudiants
+                       .OrderByDescending(t => t.ID_Etudiant)
+                       .FirstOrDefault();
 
-                if (streamInfo != null)
+                    string val1 = "NomEtudiant" + (dernierEtudiant.ID_Etudiant + 1) + ",PrenomEtudiant" + (dernierEtudiant.ID_Etudiant + 1) + ",2004-05-15,M,654879547,Etudiant" + (dernierEtudiant.ID_Etudiant + 1) + "@example.com,0";
+                    string val2 = "NomEtudiant" + (dernierEtudiant.ID_Etudiant + 2) + ",PrenomEtudiant" + (dernierEtudiant.ID_Etudiant + 2) + ",1999-12-10,F,699584123,Etudiant" + (dernierEtudiant.ID_Etudiant + 2) + "@example.com,1";
+                    string val3 = "NomEtudiant" + (dernierEtudiant.ID_Etudiant + 3) + ",PrenomEtudiant" + (dernierEtudiant.ID_Etudiant + 3) + ",2001-08-05,M,690152362,Etudiant" + (dernierEtudiant.ID_Etudiant + 3) + "@example.com,0;";
+
+                    // Créer et initialiser les lignes de valeurs par défaut
+                    List<string> lignesValeursParDefaut = new List<string>();
+                    lignesValeursParDefaut.Add(val1);
+                    lignesValeursParDefaut.Add(val2);
+                    lignesValeursParDefaut.Add(val3);
+
+                    // Écriture des lignes de valeurs par défaut dans le fichier
+                    File.WriteAllLines(filePath, lignesValeursParDefaut);
+             
+
+                // Lecture des lignes existantes
+                List<string> lignesExistantes = File.ReadAllLines(filePath).ToList();
+
+                // Traitement des lignes existantes et ajout à la base de données
+                foreach (string line in lignesExistantes)
                 {
-                    using (var reader = new System.IO.StreamReader(streamInfo.Stream))
+                    // Séparer les informations par des virgules
+                    string[] data = line.Split(',');
+
+                    if (data.Length == 7) // Vérifier si le nombre d'éléments est correct
                     {
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
+                        // Créer un nouvel étudiant avec les informations
+                        Etudiants nouvelEtudiant = new Etudiants
                         {
-                            // Séparer les informations par des virgules
-                            string[] data = line.Split(',');
+                            Nom = data[0].Trim(),
+                            Prenom = data[1].Trim(),
+                            // Utiliser TryParseExact pour gérer le format de date
+                            Date_Naissance = DateTime.TryParseExact(data[2].Trim(), "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime date) ? date : (DateTime?)null,
+                            Sexe = data[3].Trim(),
+                            Telephone = data[4].Trim(),
+                            Email = data[5].Trim(),
+                            Handicape = int.TryParse(data[6].Trim(), out int handicape) ? handicape : 0
+                        };
 
-                            if (data.Length == 7) // Vérifier si le nombre d'éléments est correct
-                            {
-                                MessageBox.Show(data[0]);
-                                // Créer un nouvel étudiant avec les informations
-                                Etudiants nouvelEtudiant = new Etudiants
-                                {
-                                    Nom = data[0].Trim(),
-                                    Prenom = data[1].Trim(),
-                                    // Utiliser TryParseExact pour gérer le format de date
-                                    Date_Naissance = DateTime.TryParseExact(data[2].Trim(), "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime date) ? date : (DateTime?)null,
-                                    Sexe = data[3].Trim(),
-                                    Telephone = data[4].Trim(),
-                                    Email = data[5].Trim(),
-                                    Handicape = int.TryParse(data[6].Trim(), out int handicape) ? handicape : 0
-                                };
-
-                                using (var context = new CiteUContext())
-                                {
-                                    context.Etudiants.Add(nouvelEtudiant);
-                                    context.SaveChanges();
-                                }
-                            }
-                            else
-                            {
-                                //MessageBox.Show("Format de ligne incorrect : " + line, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
-                            }
-                        }
-
-                        MessageBox.Show("Importation réussie", "Succès", MessageBoxButton.OK, MessageBoxImage.Information);
+                        context.Etudiants.Add(nouvelEtudiant);
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        //MessageBox.Show("Format de ligne incorrect : " + line, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
                 }
-                else
-                {
-                    //MessageBox.Show("Fichier texte introuvable en tant que ressource.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
+
+                MessageBox.Show("Importation réussie", "Succès", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Données écrites avec succès dans le fichier", "Succès", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             catch (Exception ex)
             {
-                //MessageBox.Show("Erreur lors de l'importation : " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Erreur lors de l'importation : " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
-
     }
 }
