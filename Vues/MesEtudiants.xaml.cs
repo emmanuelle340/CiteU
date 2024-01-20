5e5a9b90134ceb6a206ec93ade0f212ec02766d2
emmanuelle340 <emmanuellekenmogne340@gmail.com>
2024-01-20 13:37:30 +0100

    update

--- Vues/MesEtudiants.xaml.cs
+++ Vues/MesEtudiants.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CiteU.Modele;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CiteUContext = CiteU.Modele.CiteU;
 
 namespace CiteU.Vues
 {
@@ -24,5 +26,67 @@ namespace CiteU.Vues
         {
             InitializeComponent();
         }
+
+        private void ImporterEtudiant_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Charger le fichier texte en tant que ressource
+                Uri uri = new Uri("pack://application:,,,/CiteU;component/MesEtudiants.txt");
+                var streamInfo = Application.GetResourceStream(uri);
+
+                if (streamInfo != null)
+                {
+                    using (var reader = new System.IO.StreamReader(streamInfo.Stream))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // Séparer les informations par des virgules
+                            string[] data = line.Split(',');
+
+                            if (data.Length == 7) // Vérifier si le nombre d'éléments est correct
+                            {
+                                MessageBox.Show(data[0]);
+                                // Créer un nouvel étudiant avec les informations
+                                Etudiants nouvelEtudiant = new Etudiants
+                                {
+                                    Nom = data[0].Trim(),
+                                    Prenom = data[1].Trim(),
+                                    // Utiliser TryParseExact pour gérer le format de date
+                                    Date_Naissance = DateTime.TryParseExact(data[2].Trim(), "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime date) ? date : (DateTime?)null,
+                                    Sexe = data[3].Trim(),
+                                    Telephone = data[4].Trim(),
+                                    Email = data[5].Trim(),
+                                    Handicape = int.TryParse(data[6].Trim(), out int handicape) ? handicape : 0
+                                };
+
+                                using (var context = new CiteUContext())
+                                {
+                                    context.Etudiants.Add(nouvelEtudiant);
+                                    context.SaveChanges();
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Format de ligne incorrect : " + line, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                        }
+
+                        MessageBox.Show("Importation réussie", "Succès", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Fichier texte introuvable en tant que ressource.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de l'importation : " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
     }
 }
